f5882ff5d5e70f732d8467d6e8441036bd20526a
Andreas Marschke <andreas.marschke@gmail.com>
2014-12-24 03:15:39 +0100

    - Set CSS Hirarchy for build.min.css - add frontend assets

--- gruntconfig/files/minification.js
+++ gruntconfig/files/minification.js
@@ -18,9 +18,12 @@ module.exports.fontscopy = [
 ];
 
 module.exports.css = [
-    "assets/vendor/bootstrap/dist/css/bootstrap.min.css",
+    "assets/vendor/bootstrap/dist/css/bootstrap.css",
     "assets/vendor/font-awesome/css/font-awesome.css",
-    "assets/css/**/*.css"
+    "assets/css/base.css",
+    "assets/css/nosession.css",
+    "assets/css/usermanagement.css",
+    "assets/css/usermanagement-mq.css"
 ];
 
 module.exports.ngfiles = {
@@ -36,9 +39,10 @@ module.exports.ngfiles = {
 module.exports.js = {
     "assets/build/js/base.min.js": [
 	"assets/vendor/jquery/dist/jquery.js",
-	"assets/vendor/bootstrap/dist/js/bootstrap.js"
+	"assets/vendor/bootstrap/dist/js/bootstrap.js",
+	"assets/vendor/spin.js/spin.js"
     ],
-    "assets/build/js/angular-applications.min.js" :[
+    "assets/build/js/angular-applications.min.js": [
 	"assets/build/js/usermanagement.min.js"
     ],
     "assets/build/js/angular.min.js": [
@@ -47,15 +51,21 @@ module.exports.js = {
 	"assets/vendor/angular-ui-router/release/angular-ui-router.js",
 	"assets/vendor/angular-gettext/dist/angular-gettext.js",
 	"assets/vendor/angular-resource/angular-resource.js",
+	"assets/vendor/checklist-model/checklist-model.js",
 	"assets/build/js/angular-applications.min.js"
     ],
     "assets/build/js/graphing.min.js": [
 	"assets/vendor/d3/d3.js",
-	"assets/vendor/d3.chart/d3.chart.js"
+	"assets/vendor/d3.chart/d3.chart.js",
+	"assets/vendor/d3.chart.base/d3.chart.base.js"
     ],
     "assets/build/js/build.min.js": [
 	"assets/build/js/base.min.js",
-	"assets/build/js/angular.min.js",
-	"assets/build/js/graphing.min.js"
+	"assets/build/js/graphing.min.js",
+	"assets/build/js/angular.min.js"
+    ],
+    "assets/build/js/primer.js": [
+	"assets/vendor/modernizr/modernizr.js",
+	"assets/vendor/modernizr/feature-detects/css-mediaqueries.js"
     ]
 };
